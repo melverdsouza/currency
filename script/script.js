51b0bf210a3cd94aa499f2751707b7e5344a4f1b
Melver Dsouza <melverken10@gmail.com>
2020-07-27 18:03:04 +0530

    currency change done

--- script/script.js
+++ script/script.js
@@ -1,8 +1,10 @@
+// get data on page load
 window.onload = (event) => {
   event.preventDefault();
   getData();
 };
 
+// make fetch request on page load
 function getData() {
   let date = new Date();
   fetch(
@@ -15,13 +17,19 @@ function getData() {
 }
 
 function loadPageData(data) {
+  // add main currency dropdown
   Object.keys(data["rates"]).map((currencyName) =>
     currencyDropdown(currencyName)
   );
+  // add fluctuations for each currency
   fluxCards(data);
+  // Object.keys(data["rates"])
+  //   .filter((otherCurr) => otherCurr !== currencyList)
+  //   .map((currencyName) => conversionDisplay(currencyName));
   console.log(data);
 }
 
+// add main currency dropdown on page load
 function currencyDropdown(currencyName) {
   let currencyList = document.getElementById("currency-list");
   let optionTag = document.createElement("option");
@@ -30,7 +38,46 @@ function currencyDropdown(currencyName) {
   currencyList.appendChild(optionTag);
 }
 
+// add fluctuation card for each currency
 function fluxCards(data) {
+  let fluxCardList = document.getElementsByClassName("flux-cards-list")[0];
+  for (const curr in data["rates"]) {
+    let eachFluxCard = document.createElement("div");
+    eachFluxCard.setAttribute("class", "each-flux-card");
+    fluxCardList.appendChild(eachFluxCard);
+    let currName = document.createElement("h6");
+    currName.innerText = curr;
+    eachFluxCard.appendChild(currName);
+    let currAmt = document.createElement("h5");
+    currAmt.innerText = data["rates"][curr].toFixed(2);
+    eachFluxCard.appendChild(currAmt);
+    console.log(curr, data["rates"][curr]);
+  }
+}
+
+function conversionDisplay() {
+  let currencyList = document.getElementById("currency-list");
+  console.log(currencyList[currencyList.selectedIndex].value);
+}
+
+function changeCurrency() {
+  let currencyList = document.getElementById("currency-list");
+  console.log(currencyList[currencyList.selectedIndex].value);
+  let date = new Date();
+  fetch(
+    `https://api.ratesapi.io/api/${date.getFullYear()}-${
+      date.getMonth() + 1
+    }-${date.getDate()}?base=${currencyList[currencyList.selectedIndex].value}`
+  )
+    .then((response) => response.json())
+    .then((data) => newCurrency(data));
+}
+
+function newCurrency(data) {
+  const parent = document.getElementsByClassName("flux-cards-list")[0];
+  while (parent.firstChild) {
+    parent.firstChild.remove();
+  }
   let fluxCardList = document.getElementsByClassName("flux-cards-list")[0];
   for (const curr in data["rates"]) {
     let eachFluxCard = document.createElement("div");
